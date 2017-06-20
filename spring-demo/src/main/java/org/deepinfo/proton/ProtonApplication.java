d88ba855e5d55256e1b9aa392eb80835d5018d44
lixudong <839444394@qq.com>
2017-06-20 17:12:26 +0800

    删除 hbase ,es

--- spring-demo/src/main/java/org/deepinfo/proton/ProtonApplication.java
+++ spring-demo/src/main/java/org/deepinfo/proton/ProtonApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource(locations = {"classpath:/config/hbase-spring.xml"})
 public class ProtonApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
--- spring-demo/src/main/java/org/deepinfo/proton/controller/ExampleController.java
+++ spring-demo/src/main/java/org/deepinfo/proton/controller/ExampleController.java
@@ -1,9 +1,7 @@
 package org.deepinfo.proton.controller;
 
 import org.deepinfo.proton.model.ExampleDO;
-import org.deepinfo.proton.params.HbaseParam;
 import org.deepinfo.proton.service.ExampleService;
-import org.deepinfo.proton.util.HbaseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,7 +10,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by lixudong on 2017/4/24.
@@ -23,8 +20,6 @@ public class ExampleController {
 
     @Autowired
     private ExampleService exampleService;
-    @Autowired
-    private HbaseUtils hbaseUtils;
 
     @RequestMapping(value = "/findById/{id}", method = RequestMethod.GET)
     @ResponseBody
@@ -72,19 +67,5 @@ public class ExampleController {
     public int deleteByIds(@RequestBody List<Long> ids) {
         return exampleService.deleteByIds(ids);
     }
-
-    @RequestMapping(value = "/hbase", method = RequestMethod.GET)
-    public String testHbase() {
-        hbaseUtils.testHbase();
-        return "1";
-    }
-
-    @RequestMapping(value = "/hbase/get", method = RequestMethod.POST)
-    @ResponseBody
-    public List<Map<String, Object>> getHbase(@RequestBody HbaseParam param) {
-        List<Map<String, Object>> map = hbaseUtils.find(param.getTableName(), param.getStartRow(), param.getStopRow());
-        return map;
-    }
-
 
 }
